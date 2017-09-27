f1c64c906e81003e12de4bf0631e4b7e032aef31
chris.yang <chris.yang@weimob.com>
2017-09-27 15:02:57 +0800

    【Tabbar】Add

---
+++ wue/component/tabbar/tabbar.js
@@ -0,0 +1,39 @@
+// wue/views/tabbar/tabbar.js
+import Component from '../../base/component';
+
+class Tabbar extends Component {
+  constructor(options) {
+    super(Object.assign({}, options));
+    this.page.tapTabbar = this.tapTabbar;
+  }
+
+  show(data) {
+    let tabbar = {};
+    
+    let list = data.navList;
+    if (list instanceof Array) {
+      let tabItems = [];
+      list.forEach(item => {
+        tabItems.push(item);
+      })
+      tabbar['tabItems'] = tabItems;
+    }
+
+    tabbar['curIndex'] = 0;
+    tabbar['icon'] = tabbar['tabItems'][0].focusTitleIconUrl;
+    this.page.setData({
+      "__tabbar__": tabbar
+    });
+  }
+
+  tapTabbar(e) {
+    let index = e.currentTarget.dataset.index;
+    let icon = this.data.__tabbar__.tabItems[index].focusTitleIconUrl;
+    this.setData({
+      "__tabbar__.curIndex": index,
+      "__tabbar__.icon": icon,
+    });
+  }
+}
+
+export default Tabbar;
--- wue/wue.js
+++ wue/wue.js
@@ -3,6 +3,7 @@ import Banner from './component/banner/banner';
 import SingleImage from './component/single-image/img';
 import MultiImage from './component/multi-image/img';
 import NormalGroupGoods from './component/goods/normal-group/goods';
+import Tabbar from './component/tabbar/tabbar';
 
 /**
  * 组件集合
@@ -19,7 +20,8 @@ export default function (scope) {
     $wueBanner: new Banner(scope),
     $wueSingleImage: new SingleImage(scope),
     $wueMultiImage: new MultiImage(scope),
-    $wueNormalGroupGoods: new NormalGroupGoods(scope)
+    $wueNormalGroupGoods: new NormalGroupGoods(scope),
+    $wueTabbar: new Tabbar(scope)
   };
   wue.$wueComponentIndex = new ComponentIndex(scope, wue);
   return wue;
