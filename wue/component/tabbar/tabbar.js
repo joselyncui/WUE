88206f5b8fc7c6f5ea6359a6b719106991a6da23
chris.yang <chris.yang@weimob.com>
2017-09-28 16:42:02 +0800

    【Tabbar】自适应

--- wue/component/tabbar/tabbar.js
+++ wue/component/tabbar/tabbar.js
@@ -4,7 +4,7 @@ import Component from '../../base/component';
 class Tabbar extends Component {
   constructor(options) {
     super(Object.assign({}, options));
-    this.page.tapTabbar = this.tapTabbar;
+    this.page.tapComponentTabbar = this.tapComponentTabbar;
   }
 
   show(data) {
@@ -22,11 +22,11 @@ class Tabbar extends Component {
     tabbar['curIndex'] = 0;
     tabbar['icon'] = tabbar['tabItems'][0].focusTitleIconUrl;
     this.page.setData({
-      "__tabbar__": tabbar
+      "__tabbar__": tabbar,
     });
   }
 
-  tapTabbar(e) {
+  tapComponentTabbar(e) {
     let index = e.currentTarget.dataset.index;
     let icon = this.data.__tabbar__.tabItems[index].focusTitleIconUrl;
     this.setData({
